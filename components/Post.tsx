0a54855b210089adc88cc1f0b2730194a1db9deb
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-06-06 18:14:26 -0400

    started making drop down

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,11 +1,79 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 import Card from './Card'
 import Avatar from './Avatar'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Post() {
+    const [dropDown, setDropDown] = useState(false);
+
+    const handleClick = () => {
+        setDropDown(!dropDown)
+    }
+
     return (
         <Card>
-            <Avatar />
+            <div className='flex gap-3 w-full items-start relative'>
+                <Avatar />
+                <div>
+                    <p><Link href={''} className='font-semibold'>Jon Doe</Link> shared an <Link href={''} className='text-blue-500'>album</Link></p>
+                    <p className='text-gray-500 text-sm'>2 hours ago</p>
+                </div>
+                <button className='ml-auto text-gray-500' onClick={handleClick}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                    </svg>
+                </button>
+                <div className={`absolute shadow bg-white z-10 ${dropDown ? 'show': 'hidden'}`}>
+                    Hello world
+                </div>
+            </div>
+            <div>
+                <p className='my-3 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ex hic facere, facilis reiciendis corrupti beatae ullam, accusantium ad harum, pariatur ipsa. Laboriosam consequatur ratione sint dolores. Tempora, optio sunt!</p>
+                <div className='rounded-md w-full overflow-hidden'>
+                    <Image 
+                        src={'https://images.unsplash.com/photo-1510414842594-a61c69b5ae57?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'} 
+                        alt={''}
+                        height={500}
+                        width={500}
+                        className='object-cover w-full'
+                    />    
+                </div>     
+            </div>
+            <div className='mt-4 flex gap-8'>
+                <button className='flex gap-2 items-center'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                    </svg>
+                    72
+                </button>
+                <button className='flex gap-2 items-center'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+                    </svg>
+                    11
+                </button>
+                <button className='flex gap-2 items-center'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z" />
+                    </svg>
+                    Share
+                </button>
+            </div>
+            <div className='flex gap-3 mt-4'>
+                <Avatar />
+                <div className='border rounded w-full relative h-12'>
+                    <textarea className='p-3' placeholder='Leave a comment' rows={1} />
+                    <button className='absolute top-3 right-3 text-gray-400'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
         </Card>
     )
 }

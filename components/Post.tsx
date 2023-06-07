220803b5e803b8417d10a2ed4c4b0e0c8a5df168
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-06-06 21:20:32 -0400

    made dropdown

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,17 +1,27 @@
 "use client"
 
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Card from './Card'
 import Avatar from './Avatar'
 import Link from 'next/link'
 import Image from 'next/image'
 
 export default function Post() {
+    const menu = useRef(null);
     const [dropDown, setDropDown] = useState(false);
 
     const handleClick = () => {
         setDropDown(!dropDown)
     }
+
+    const closeOpenMenus = (e: any) => {
+        //@ts-ignore
+        if (menu.current && dropDown && !menu.current.contains(e.target)){
+            setDropDown(false)
+        }
+    }
+
+    document.addEventListener('mousedown', closeOpenMenus)
 
     return (
         <Card>
@@ -26,8 +36,39 @@ export default function Post() {
                         <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                     </svg>
                 </button>
-                <div className={`absolute shadow bg-white z-10 ${dropDown ? 'show': 'hidden'}`}>
-                    Hello world
+                <div className={`absolute bg-white top-6 -right-6 p-3 shadow-md shadow-gray-300 rounded-sm border border-gray-100 ${dropDown ? 'opacity-100': 'opacity-0'} transition-all duration-150`}>
+                    <div ref={menu} className={`${dropDown ? 'opactity-100' : 'opacity-0'}`}>
+                        <button className='flex items-center gap-2 py-2 px-2'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
+                            </svg>
+                            Save post
+                        </button>
+                        <button className='flex items-center gap-2 py-2 px-2'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5" />
+                            </svg>
+                            Turn on notifications
+                        </button>
+                        <button className='flex items-center gap-2 py-2 px-2'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
+                            </svg>
+                            Hide post
+                        </button>
+                        <button className='flex items-center gap-2 py-2 px-2 text-red-500/40 hover:text-red-500 transition-colors duration-200'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                            </svg>
+                            Delete
+                        </button>
+                        <button className='flex items-center gap-2 py-2 px-2'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                            </svg>
+                            Report
+                        </button>
+                    </div>
                 </div>
             </div>
             <div>
@@ -65,7 +106,7 @@ export default function Post() {
             <div className='flex gap-3 mt-4'>
                 <Avatar />
                 <div className='border rounded w-full relative h-12'>
-                    <textarea className='p-3' placeholder='Leave a comment' rows={1} />
+                    <textarea className='p-3 w-full' placeholder='Leave a comment' rows={1} />
                     <button className='absolute top-3 right-3 text-gray-400'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />

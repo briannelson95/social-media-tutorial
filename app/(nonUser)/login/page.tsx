0c6cd6ca92434dc734ee8bef3382c01f750a0943
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-06-12 09:18:27 -0400

    redirect if no session

--- app/(nonUser)/login/page.tsx
+++ app/(nonUser)/login/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function page() {
     return (
         <main className='h-screen flex items-center justify-center'>
-            <section className='max-w-md w-full'>
+            <section className='max-w-sm w-full'>
                 <h1 className='text-4xl text-gray-400 text-center mb-4'>Login</h1>
                 <LoginCard />
             </section>
